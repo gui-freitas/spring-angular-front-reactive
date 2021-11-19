4c64445bc8272167deba2e18e820ecacd83ba57a
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-11-19 09:20:00 -0300

    Criacao formulario de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ClienteService } from './cliente.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ClienteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
